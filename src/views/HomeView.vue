90f300cb8e7edf6d3dfc61f94bfc5366af014844
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-15 19:47:26 +0100

    Updated HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="home">
     <h1>Hello World</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click ="handleClick">click me</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
+    console.log(this)
+
+    const p = ref(null)
     let name = 'lucy'
     let age = 30
 
     const handleClick = () => {
-      console.log('you clicked me')
+      console.log(p,p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello'
     }
 
-    return {name, age, handleClick}
+    return {name, age, handleClick, p}
   }
 }
 </script>
