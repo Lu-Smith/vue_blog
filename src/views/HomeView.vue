c8843f2101010c09ed2b04a106e4ed8d7cc2c554
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-16 19:56:04 +0100

    Created PostList.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <PostList />
   </div>
 </template>
 
 <script>
+import PostList from '../components/PostList.vue'
 // @ is an alias to /src
 import {ref} from 'vue'
 
 export default {
   name: 'HomeView',
+  components: {
+    PostList
+  },
   setup() {
     const posts = ref([
       {title: 'welcome to the blog', body: 'Lorem ipsum', id: 1},
       {title: 'top 5 css', body: 'Lorem ipsum', id: 2},
     ])
-    return { posts}
+    return { posts }
   }
 }
 </script>
