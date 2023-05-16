9494282e9fb848be0b73839def8409db55ea3da5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-16 18:33:15 +0100

    Updated HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,21 @@
 <template>
   <div class="home">
-    <h1>Refs</h1>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click ="updateNinjaOne">Update Ninja one</button>
-    <h1>Rective</h1>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click ="updateNinjaTwo">Update Ninja two</button>
+    <h1>Home</h1>
   </div>
+  <p>{{ name }}</p>
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref, reactive } from 'vue'
+import { computed, ref} from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
-
-    const ninjaOne = ref({ name:'mario', age: 30})
-    const ninjaTwo = reactive({ name:'luna', age: 35})
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
-    }
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 40
-    }
-
-    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo }
+    const name = computed(() => {
+      return 'shaun'
+    })
+    return { name }
   }
 }
 </script>
