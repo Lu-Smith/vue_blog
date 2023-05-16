d530c63455602a7891dda1ed0f1fabf26fa71002
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-16 18:21:00 +0100

    Added reactive logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,24 +3,28 @@
     <h1>Refs</h1>
     <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
     <button @click ="updateNinjaOne">Update Ninja one</button>
+    <h1>Rective</h1>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click ="updateNinjaOne">Update Ninja one</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
 
     const ninjaOne = ref({ name:'mario', age: 30})
+    const ninjaTwo = reactive({ name:'luna', age: 35})
 
     const updateNinjaOne = () => {
       ninjaOne.value.age = 40
     }
 
-    return { ninjaOne, updateNinjaOne }
+    return { ninjaOne, ninjaTwo, updateNinjaOne }
   }
 }
 </script>
