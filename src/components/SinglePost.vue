813b0acf47e5645a7f8ec72faa1ae69241b0a3ce
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-17 16:24:08 +0100

    Updated SinglePositive.vue

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,13 +1,21 @@
 <template>
     <div class="post">
         <h2>{{ post.title }}</h2>
-        <p>{{ post.body }}</p>
+        <p>{{ snippet }}</p>
     </div>
 </template>
 
 <script>
+import { computed } from 'vue'
+
 export default {
-props: ['post']
+props: ['post'],
+setup(props) {
+    const snippet = computed(() => {
+        return props.post.body.substring(0, 100) + '....'
+    })
+    return { snippet }
+}
 }
 </script>
 
