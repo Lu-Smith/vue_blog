203bbd1dfa8f02278f0474a3ff1e32460d17e2de
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-18 19:57:29 +0100

    Updated HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,10 +18,21 @@ export default {
     PostList
   },
   setup() {
-    const posts = ref([
-      {title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus alias animi suscipit error numquam ea ratione? Fugit nesciunt voluptatibus iste saepe sunt dolor suscipit doloribus eius molestias debitis, accusantium commodi rem illo dolores velit voluptates corrupti sapiente qui. Dignissimos est et laboriosam quisquam enim expedita obcaecati, ducimus quod, maxime, at minus reiciendis quasi ipsum totam voluptates officia fugiat reprehenderit accusantium sunt? Ducimus voluptate suscipit repellat voluptates reiciendis voluptatibus amet hic labore ratione fugiat reprehenderit quaerat quasi odit tempore inventore consequuntur minus molestias, laborum maxime? Adipisci iure minus soluta vitae aspernatur quod sunt commodi, obcaecati temporibus quis inventore consectetur asperiores corrupti!', id: 1},
-      {title: 'top 5 css', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus alias animi suscipit error numquam ea ratione? Fugit nesciunt voluptatibus iste saepe sunt dolor suscipit doloribus eius molestias debitis, accusantium commodi rem illo dolores velit voluptates corrupti sapiente qui. Dignissimos est et laboriosam quisquam enim expedita obcaecati, ducimus quod, maxime, at minus reiciendis quasi ipsum totam voluptates officia fugiat reprehenderit accusantium sunt? Ducimus voluptate suscipit repellat voluptates reiciendis voluptatibus amet hic labore ratione fugiat reprehenderit quaerat quasi odit tempore inventore consequuntur minus molestias, laborum maxime? Adipisci iure minus soluta vitae aspernatur quod sunt commodi, obcaecati temporibus quis inventore consectetur asperiores corrupti!', id: 2},
-    ])
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async() => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        console.log(data)
+      }
+      catch(err) {
+
+      }
+    }
+
+    load()
+
     const showPosts = ref(true)
 
     return { posts, showPosts }
