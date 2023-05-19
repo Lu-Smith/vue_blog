76bb5d85a6172a8eb8fb68b469bbf5efc45f3c1c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-19 10:35:51 +0100

    Created getPosts.js

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
 
 <script>
 import PostList from '../components/PostList.vue'
-import {ref} from 'vue'
+import getPosts from '../composables/getPosts'
 
 export default {
   name: 'HomeView',
@@ -17,25 +17,9 @@ export default {
     PostList
   },
   setup() {
-    const posts = ref([])
-    const error = ref(null)
-
-    const load = async() => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if(!data.ok) {
-          throw Error('no data available')
-        }
-        posts.value = await data.json()
-      }
-      catch(err) {
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
-
+    
+    const {posts, error, load} = getPosts()
     load()
-
     return { posts, error }
   }
 }
